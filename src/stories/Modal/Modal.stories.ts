d5c585c11c07bd91c24829a10c6bcabf18b68747
Fernando Escobar <57463087+FerEscobarDev@users.noreply.github.com>
2024-12-01 13:15:41 -0500

    update

--- src/stories/Modal/Modal.stories.ts
+++ src/stories/Modal/Modal.stories.ts
@@ -1,33 +1,23 @@
-import { useState } from "react";
 import { Modal } from "../../components";
 import { Meta, StoryObj } from "@storybook/react";
-import { ContextModal } from "@context/contextModal";
 
-const meta = {
+const meta: Meta<typeof Modal> = {
     title: 'SincoSgd/Modal',
     component: Modal,
     parameters: {
         layout: 'centered',
     },
-} satisfies Meta<typeof Modal>;
+};
 
 export default meta;
 
 type Story = StoryObj<typeof Modal>;
 
-const ModalProviderDecorator = (Story: any) => {
-    const [isOpen, setIsOpen] = useState(true);
-    const handleCierre = () => setIsOpen(false);
-    return (
-        <ContextModal.Provider value={{ modal: { open: isOpen }, handleCierre }}>
-            <Story />
-        </ContextModal.Provider>
-    );
-};
-
 export const Basic: Story = {
     args: {
         open: true,
+        headerProps: {
+            titulo: 'Modal Title',
+        },
     },
-    decorators: [ModalProviderDecorator],
 };
